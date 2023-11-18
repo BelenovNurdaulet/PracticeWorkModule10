050ad0eb33fdb2334748a0973fd800a93cb68db0
Нурдаулет Беленов <belenovnurdaulet179@gmail.com>
2023-11-18 20:10:40 +0600

    Добавьте файлы проекта.

---
+++ DOP/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOP
+{
+    public interface IStudent
+    {
+        string Name { get; set; }
+        string FullName { get; set; }
+        int[] Grades { get; set; }
+        string GetName();
+        string GetFullName();
+        double GetAvgGrade();
+    }
+
+    public class Student : IStudent
+    {
+        public string Name { get; set; }
+        public string FullName { get; set; }
+        public int[] Grades { get; set; }
+
+        public Student(string name, string fullName, int[] grades)
+        {
+            Name = name;
+            FullName = fullName;
+            Grades = grades;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public string GetFullName()
+        {
+            return FullName;
+        }
+
+        public double GetAvgGrade()
+        {
+            if (Grades.Length == 0)
+            {
+                return 0.0;
+            }
+
+            double sum = 0;
+            foreach (int grade in Grades)
+            {
+                sum += grade;
+            }
+
+            return sum / Grades.Length;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            int[] studentGrades = { 71, 77, 71, 75, 78 };
+            IStudent student = new Student("Нурик", "Беленов Нурдаулет", studentGrades);
+
+            Console.WriteLine($"Имя: {student.Name}");
+            Console.WriteLine($"Полное имя: {student.FullName}");
+            Console.WriteLine($"Оценки: [{string.Join(", ", student.Grades)}]");
+            Console.WriteLine($"Средний балл: {student.GetAvgGrade()}");
+            Console.WriteLine("Чего хочет: Стипендию ");
+
+        }
+    }
+}
---
+++ PracticeWorkModule10/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeWorkModule10
+{
+    //Интерфейсы для частей дома и рабочих
+    interface IPart
+    {
+        void Build();
+    }
+
+    interface IWorker
+    {
+        void Work(House house);
+    }
+
+    // Для частей дома
+    class Basement : IPart
+    {
+        public void Build()
+        {
+            Console.WriteLine("Фундамент построен.");
+        }
+    }
+
+    class Wall : IPart
+    {
+        public void Build()
+        {
+            Console.WriteLine("Стена построена.");
+        }
+    }
+
+    class Door : IPart
+    {
+        public void Build()
+        {
+            Console.WriteLine("Дверь построена.");
+        }
+    }
+
+    class Window : IPart
+    {
+        public void Build()
+        {
+            Console.WriteLine("Окно построено.");
+        }
+    }
+
+    class Roof : IPart
+    {
+        public void Build()
+        {
+            Console.WriteLine("Крыша построена.");
+        }
+    }
+
+
+    class House
+    {
+        private List<IPart> parts = new List<IPart>();
+
+        public void AddPart(IPart part)
+        {
+            parts.Add(part);
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Дом построен. Вот его состав:");
+
+            foreach (var part in parts)
+            {
+                part.Build();
+            }
+        }
+    }
+
+    // Для бригады строителей
+    class Worker : IWorker
+    {
+        public void Work(House house)
+        {
+            house.AddPart(new Basement());
+            house.AddPart(new Wall());
+            house.AddPart(new Wall());
+            house.AddPart(new Wall());
+            house.AddPart(new Wall());
+            house.AddPart(new Door());
+            house.AddPart(new Window());
+            house.AddPart(new Window());
+            house.AddPart(new Window());
+            house.AddPart(new Window());
+            house.AddPart(new Roof());
+        }
+    }
+
+    class TeamLeader : IWorker
+    {
+        public void Work(House house)
+        {
+            house.Show();
+        }
+    }
+
+    class Team
+    {
+        private List<IWorker> workers = new List<IWorker>();
+
+        public void AddWorker(IWorker worker)
+        {
+            workers.Add(worker);
+        }
+
+        public void BuildHouse(House house)
+        {
+            foreach (var worker in workers)
+            {
+                worker.Work(house);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            House house = new House();
+            Team team = new Team();
+            team.AddWorker(new Worker());
+            team.AddWorker(new Worker());
+            team.AddWorker(new Worker());
+            team.AddWorker(new Worker());
+            team.AddWorker(new TeamLeader());
+
+            team.BuildHouse(house);
+        }
+    }
+}
